4ca9994ba687d329f2e70b1e736e421b069e585a
Ron Lin <ronrlin@gmail.com>
2015-04-15 17:37:07 -0400

    refactoring to php

---
+++ footer.php
@@ -0,0 +1,49 @@
+        <div id='footer-main'>
+          <div class='container'>
+            <div class='row'>
+              <div class='col-md-3 col-sm-6 info-box'>
+                <div class='logo-container'>
+                  <img alt="Jednotka" width="117" height="39" src="assets/images/omni-logo.png" />
+                </div>
+                <p class='no-mg-b'>We're a non-profit developing the nanotechnology curriculum for high school students.</p>
+                <br />
+                <p>Omni Nano is here to inspire the scientists, entrepreneurs and engineers of tomorrow.</p>
+              </div>
+              <div class='col-md-3 col-sm-6 info-box'>
+                <h2 class='title'>For Educators</h2>
+                <div>
+                  <p><a href="educators.html">Discover Program</a></p>
+                  <p><a href="educators.html">Bridges Program</a></p>
+                  <p><a href="educators.html">The Curriculum</a></p>
+                  <p><a href="testimonials.html">Testimonials</a></p>                  
+                </div>
+                <h2 class='title'>Media Relations</h2>
+                <div>
+                  <p><a href="press.html">Media Relations</a></p>
+                  <p><a href="press.html">Press Releases</a></p>
+                  <p><a href="press.html">Press Links</a></p>
+                  <p><a href="#">Blog</a></p>                  
+                </div>
+              </div>
+`              <div class='col-md-3 col-sm-6 info-box social-box'>
+                <h2 class='title'>Support Omni Nano</h2>
+                <p><a href="donate.html">Donate</a></p>
+                <p><a href="partnerships.html">Corporate Support</a></p>
+                <p><a href="about_us.html">Why Omni Nano?</a></p>
+                <p><a href="faq.html">FAQ</a></p>
+                <p><a href="legal.html">Legal</a></p>
+              </div>
+              <div class='col-md-3 col-sm-6 info-box'>
+                <div>
+                  <h2 class='title'>OmniNano.org</h2>
+                  <p><a href="">Mission</a></p>
+                  <p><a href="about_us.html">About us</a></p>
+                  <p><a href="about_us.html">Leadership</a></p>
+                  <p><a href="about_us.html">Board of Directors</a></p>
+                  <p><a href="partnerships.html">Partnerships</a></p>
+                  <p><a href="awards.html">Awards</a></p> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
